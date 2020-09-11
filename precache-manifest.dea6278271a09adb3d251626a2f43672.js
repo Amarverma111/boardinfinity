5248e7f6d06078a52d27135ae81f19173f3e24ee
unknown <amar9852@gmail.com>
2020-09-11 15:11:19 -0400

    Updates

---
+++ precache-manifest.dea6278271a09adb3d251626a2f43672.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "834ac3c745758d5a00025da3c7e948b5",
+    "url": "/ReactProjectfirst/index.html"
+  },
+  {
+    "revision": "7dea6bfc906d367dcca5",
+    "url": "/ReactProjectfirst/static/css/main.d6ae19e5.chunk.css"
+  },
+  {
+    "revision": "38d91c90cb691381eab3",
+    "url": "/ReactProjectfirst/static/js/2.2c3dd8fe.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ReactProjectfirst/static/js/2.2c3dd8fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dea6bfc906d367dcca5",
+    "url": "/ReactProjectfirst/static/js/main.029f0d20.chunk.js"
+  },
+  {
+    "revision": "f6fba4860cb5a879a96a",
+    "url": "/ReactProjectfirst/static/js/runtime-main.3d33c177.js"
+  }
+]);
